79e7794751b1c9d6e27b391f0c47b40f7d0ba0f8
Octavio Fernandez <fernandez.octavio.ismael@gmail.com>
2026-04-26 20:05:01 -0300

    mejoramos todo

--- models/__init__.py
+++
@@ -1,3 +0,0 @@
-from electriapp.models.cliente import Cliente
-from electriapp.models.trabajo import Trabajo
-__all__ = ["Cliente", "Trabajo"]
--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="electriapp",
-    version="0.1.0",
-    packages=find_packages(),
-)
